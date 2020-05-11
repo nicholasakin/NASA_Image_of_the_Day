1d0a209bbe82d5d3d4a8a241aa3400447d4525c2
Nika_Kcin <nicholasakin@gmail.com>
2020-05-11 14:44:47 -0400

    Updated App

--- app/src/main/java/akin/person/nasaimageoftheday/MainActivity.java
+++ app/src/main/java/akin/person/nasaimageoftheday/MainActivity.java
@@ -200,6 +200,7 @@ public class MainActivity extends AppCompatActivity {
                     getWebContents(urlLink);
 
 
+
                     //resets variables
                     date = "";
                     urlLink = "";
